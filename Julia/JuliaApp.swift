5d1c5c30355ed482daba94b6cb24c652874da100
Robin Willis <robincwillis@gmail.com>
2024-06-19 12:03:14 -0700

    Initial Commit

---
+++ Julia/JuliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuliaApp.swift
+//  Julia
+//
+//  Created by Robin Willis on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct JuliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
